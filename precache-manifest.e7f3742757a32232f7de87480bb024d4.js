d8ff7afee57d9096abd0a7e311c2bd16ed061ace
Frankie Riviera <frankgriviera@outlook.com>
2020-08-07 15:48:46 -0400

    Automation Script Deploy: 08/07/2020 15:48:46

---
+++ precache-manifest.e7f3742757a32232f7de87480bb024d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd9c13a623024c5e90811249538c5fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e130c800318a9696eebe",
+    "url": "/static/css/2.3c2657b0.chunk.css"
+  },
+  {
+    "revision": "66eebfdb08816b89122e",
+    "url": "/static/css/main.0c26f042.chunk.css"
+  },
+  {
+    "revision": "e130c800318a9696eebe",
+    "url": "/static/js/2.d7c4f413.chunk.js"
+  },
+  {
+    "revision": "85d965a067415930481a1b5c455d0527",
+    "url": "/static/js/2.d7c4f413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66eebfdb08816b89122e",
+    "url": "/static/js/main.ee60fa9d.chunk.js"
+  },
+  {
+    "revision": "7f068d4fbc13d15168c6",
+    "url": "/static/js/runtime-main.c3d895a1.js"
+  }
+]);
